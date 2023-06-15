ec3f361e67f776eb0b09d7775bc89bf5631af553
M-NabeelAli <49489233+M-NabeelAli@users.noreply.github.com>
2023-06-15 18:56:03 +0500

    change root directory

---
+++ Sources/UStoryPackage/HXPHPicker/Picker.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by imac3 on 15/06/2023.
+//
+
+import Foundation
+import UIKit
+
+public class Picker{
+    
+    static let shared = Picker()
+   
+    private init() {
+        
+    }
+}
